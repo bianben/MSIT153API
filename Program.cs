46d3acc8d50503910cb919f0510ff6d4cf389788
bian <bute77889@gmail.com>
2023-11-17 15:52:12 +0800

    加入專案檔案。

---
+++ Program.cs
@@ -0,0 +1,38 @@
+using Microsoft.EntityFrameworkCore;
+using MSIT153API.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers().AddXmlSerializerFormatters();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<NorthwindContext>(
+    options => options.UseSqlServer(
+ builder.Configuration.GetConnectionString("dbContext")
+));
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        builder => builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
+});
+
+var app = builder.Build();
+app.UseCors("AllowAll");
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
